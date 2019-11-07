291d616757b75474c9b399b0b2a5aebeb634a81b
ericc70 <eric.c@codeur.online>
2019-11-07 12:56:54 +0000

    map part

---
+++ js/map.js
@@ -0,0 +1,6 @@
+var mymap = L.map('mapid').setView([47.6121, 6.1646], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
