b73b38c779c99d750ff620175043b97012d2376f
ericc70 <eric.c@codeur.online>
2019-11-07 14:00:00 +0000

    contact part

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([47.6121, 6.1646], 13);
+var mymap = L.map('mapid').setView([47.6121, 6.1646], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 maxZoom: 19,
